f4753fefc976a8d45971777f6445dbb68dfbba48
MadStorm <stormlunix@gmail.com>
2015-11-06 20:00:36 +0100

    Added Package Course Help and Activities
    Added Pacakage Course Help
    Added CourseInforMenu Activity

---
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/coursehelp/CourseInfoMenuActivity.java
@@ -0,0 +1,15 @@
+package lukelunix.ntnumple.coursehelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lukelunix.ntnumple.R;
+
+public class CourseInfoMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_info_menu);
+    }
+}
