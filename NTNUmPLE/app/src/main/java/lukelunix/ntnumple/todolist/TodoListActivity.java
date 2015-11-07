dc9fe31d8155c74a5731b7729e8e6f9e36e9ffa2
MadStorm <stormlunix@gmail.com>
2015-11-07 13:10:18 +0100

    Added Tools Activity
    Added Tools Activity Java and XML skeleton

---
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/todolist/TodoListActivity.java
@@ -0,0 +1,15 @@
+package lukelunix.ntnumple.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lukelunix.ntnumple.R;
+
+public class TodoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+    }
+}
---
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/ToolsActivity.java
@@ -0,0 +1,15 @@
+package lukelunix.ntnumple.tools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lukelunix.ntnumple.R;
+
+public class ToolsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tools);
+    }
+}
