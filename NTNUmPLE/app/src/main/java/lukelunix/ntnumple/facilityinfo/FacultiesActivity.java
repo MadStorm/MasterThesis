00226d1849cb73bab1953459407a7ce6da3238eb
MadStorm <stormlunix@gmail.com>
2015-11-25 18:48:50 +0100

    Added All Java Classes for Faculties
    Added FacultiesActivity - View
    Added FacultiesData - Model
    Added FacultyAdapter - Controller - adapter for the listview

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/facilityinfo/FacultiesActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/facilityinfo/FacultiesActivity.java
@@ -3,23 +3,41 @@ package lukelunix.ntnumple.facilityinfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import lukelunix.ntnumple.R;
 
 /**
  * Created by Luka on 24.11.2015.
  */
-public class FacultiesActivity extends AppCompatActivity implements View.OnClickListener {
+public class FacultiesActivity extends AppCompatActivity{
+
+    //Declare global variables for FacultiesActivity
+    private ExpandableListView expFacultyList;
+    private HashMap<String, List<String>> facultyData;
+    private List<String> facultyList;
+    private FacultyAdapter fadapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faculty);
-    }
 
-    @Override
-    public void onClick(View v) {
+        //Initialize global variables
+        expFacultyList = (ExpandableListView) findViewById(R.id.listViewFaculty);
+        facultyData = FacultiesData.getData();
+        facultyList = new ArrayList<String>(facultyData.keySet());
+        fadapter = new FacultyAdapter(this, facultyData, facultyList);
+
+        //Set FacultyAdapter to ExpandableListView
+        expFacultyList.setAdapter(fadapter);
 
     }
 }
