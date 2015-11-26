ff929e3bdb85574a4e90b8ef2686aac2a5e3a41c
MadStorm <stormlunix@gmail.com>
2015-11-26 21:19:20 +0100

    Added Java classes for studentservice
    Added StudentserviceActivity - View
    Added StudetnserviceData - Model
    Added Studentservice - Controller

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/facilityinfo/studentservice/StudentserviceActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/facilityinfo/studentservice/StudentserviceActivity.java
@@ -2,6 +2,11 @@ package lukelunix.ntnumple.facilityinfo.studentservice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 import lukelunix.ntnumple.R;
 
@@ -10,10 +15,26 @@ import lukelunix.ntnumple.R;
  */
 public class StudentserviceActivity extends AppCompatActivity {
 
+    //Declare global variables for FacultiesActivity
+    private ExpandableListView expStudentserviceList;
+    private TreeMap<String, List<String>> studentserviceData;
+    private List<String> studentserviceList;
+    private StudentserviceAdapter sadapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentservice);
+
+        //Initialize global variables
+        expStudentserviceList = (ExpandableListView) findViewById(R.id.listViewStudentservice);
+        studentserviceData = StudentserviceData.getData();
+        studentserviceList = new ArrayList<String>(studentserviceData.keySet());
+        sadapter = new StudentserviceAdapter(this, studentserviceData, studentserviceList);
+
+        //Set StudentserviceAdapter to ExpandableListView
+        expStudentserviceList.setAdapter(sadapter);
     }
 
 }
