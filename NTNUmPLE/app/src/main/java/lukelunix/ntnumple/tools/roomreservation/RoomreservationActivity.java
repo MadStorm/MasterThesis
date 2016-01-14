17c3236e18bf222d97c37d839ddcf2892dbcd0bb
MadStorm <stormlunix@gmail.com>
2016-01-14 20:08:11 +0100

    Added RoomreservationActivity and XML file
    Added Webview for roomreservation
    Added URL address
    Added XML file with webview layout

---
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/roomreservation/RoomreservationActivity.java
@@ -0,0 +1,25 @@
+package lukelunix.ntnumple.tools.roomreservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import lukelunix.ntnumple.R;
+
+public class RoomreservationActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roomreservation);
+
+        webview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("https://romres.ntnu.no/2016/Login.aspx?ReturnUrl=%2f2016%2f");
+    }
+}
