526287d54bc4c425d6ffd5fdfbb5bd7d1cde9ec8
MadStorm <stormlunix@gmail.com>
2016-01-14 20:06:43 +0100

    Added MazemapActivity

---
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/mazemap/MazemapActivity.java
@@ -0,0 +1,15 @@
+package lukelunix.ntnumple.tools.mazemap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lukelunix.ntnumple.R;
+
+public class MazemapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mazemap);
+    }
+}
