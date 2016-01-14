dcdcf2e46ecad3024499ed4b562836fcb25bf12b
MadStorm <stormlunix@gmail.com>
2016-01-14 20:06:22 +0100

    Updated ToolsActivity Menu
    Added onclicklistener for mazemap, kahoot and roomreservation.
    Added functions for onclick event

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/ToolsActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/ToolsActivity.java
@@ -1,15 +1,62 @@
 package lukelunix.ntnumple.tools;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import lukelunix.ntnumple.R;
+import lukelunix.ntnumple.tools.mazemap.MazemapActivity;
+import lukelunix.ntnumple.tools.roomreservation.RoomreservationActivity;
 
-public class ToolsActivity extends AppCompatActivity {
+public class ToolsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Tools menu buttons
+    private ImageButton mazemap;
+    private ImageButton kahoot;
+    private ImageButton roomreservation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tools);
+        mazemap = (ImageButton) findViewById(R.id.imageButtonMazeMap);
+        mazemap.setOnClickListener(this);
+        kahoot = (ImageButton) findViewById(R.id.imageButtonKahoot);
+        kahoot.setOnClickListener(this);
+        roomreservation = (ImageButton) findViewById(R.id.imageButtonRoomReservation);
+        roomreservation.setOnClickListener(this);
+
+
     }
+
+    private void mazemapClick(){
+        startActivity(new Intent(getApplicationContext(), MazemapActivity.class));
+    }
+
+    private void kahootClick(){
+
+    }
+
+    private void roomreservationClick(){
+        startActivity(new Intent(getApplicationContext(), RoomreservationActivity.class));
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageButtonMazeMap:
+                mazemapClick();
+                break;
+            case R.id.imageButtonKahoot:
+                kahootClick();
+                break;
+            case R.id.imageButtonRoomReservation:
+                roomreservationClick();
+                break;
+        }
+    }
+
 }
