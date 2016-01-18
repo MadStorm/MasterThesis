c88cba861a0ffb3f5383499dba6e7507a68baeab
MadStorm <stormlunix@gmail.com>
2016-01-18 09:28:19 +0100

    Added Mazemap Java and XML
    Added MazemapActivity
    Added activity_mazemap

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/mazemap/MazemapActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/mazemap/MazemapActivity.java
@@ -2,14 +2,24 @@ package lukelunix.ntnumple.tools.mazemap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import lukelunix.ntnumple.R;
 
 public class MazemapActivity extends AppCompatActivity {
 
+    private WebView webview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mazemap);
+
+        webview = (WebView) findViewById(R.id.webviewmazemap);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("http://use.mazemap.com");
     }
 }
