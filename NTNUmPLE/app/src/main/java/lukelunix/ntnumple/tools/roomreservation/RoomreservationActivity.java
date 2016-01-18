d4cfbec78447fb767d0379bc8a3b9aa1c4a94abf
MadStorm <stormlunix@gmail.com>
2016-01-18 09:29:03 +0100

    Added room reservation Java and XML
    Added RoomreservationActivity
    Added activity_roomreservation

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/roomreservation/RoomreservationActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/tools/roomreservation/RoomreservationActivity.java
@@ -11,13 +11,12 @@ public class RoomreservationActivity extends AppCompatActivity {
 
     private WebView webview;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roomreservation);
 
-        webview = (WebView) findViewById(R.id.webview);
+        webview = (WebView) findViewById(R.id.webviewroomreservation);
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webview.loadUrl("https://romres.ntnu.no/2016/Login.aspx?ReturnUrl=%2f2016%2f");
