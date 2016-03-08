813552b3db9ea060a5e11abe09389551565ba05c
MadStorm <stormlunix@gmail.com>
2016-03-08 17:37:51 +0100

    Added overflow menu to main menu
    Added overflow menu to main menu where user can give feedback whenever
    he or she wants to

--- NTNUmPLE/app/src/main/java/lukelunix/ntnumple/mainmenu/MainActivity.java
+++ NTNUmPLE/app/src/main/java/lukelunix/ntnumple/mainmenu/MainActivity.java
@@ -1,24 +1,28 @@
 package lukelunix.ntnumple.mainmenu;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.ImageButton;
-import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 import lukelunix.ntnumple.R;
-import lukelunix.ntnumple.coursehelp.CoursehelpActivity;
 import lukelunix.ntnumple.contact.ContactActivity;
+import lukelunix.ntnumple.coursehelp.CoursehelpActivity;
 import lukelunix.ntnumple.feedback.Feedback;
-import lukelunix.ntnumple.todolist.TodoListActivity;
+import lukelunix.ntnumple.feedback.FeedbackInMenu;
 import lukelunix.ntnumple.links.LinksActivity;
+import lukelunix.ntnumple.todolist.TodoListActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //Main menu buttons
+    //Main todolistmenu buttons
     private ImageButton coursehelp;
     private ImageButton facilitinfo;
     private ImageButton todolist;
@@ -41,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         todolist.setOnClickListener(this);
         tools = (ImageButton)findViewById(R.id.imageButtonTools);
         tools.setOnClickListener(this);
+        getOverflowMenu();
     }
 
     private void courseHelpClick(){
@@ -76,5 +81,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
         }
+    }
+
+    private void getOverflowMenu() {
+
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.feedbackmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Main menu menu - feedback
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.buttonFeedback:
+                new FeedbackInMenu().showFeedbackDialog(this);
+                break;
+        }
+        return (super.onOptionsItemSelected(menuItem));
     }
 }
